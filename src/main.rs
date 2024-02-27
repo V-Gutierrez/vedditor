6ecbaf6b457e16118d7960641f6009b4d912258e
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-27 19:37:31 -0300

    feat: add robust char input handling

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
-use std::io::{stdin, Read};
+use std::io::{stdin, stdout, Read};
+
+use termion::raw::IntoRawMode;
+
+fn to_ctrl_byte(c: char) -> u8 {
+    let byte = c as u8;
+    
+    // bitwise AND operation
+    byte & 0b0001_1111
+}
+
+fn finish_with_error(e: std::io::Error) {
+    panic!("{}", e);
+}
 
 fn main() {
+    let _stdout = stdout().into_raw_mode().unwrap();
+
     for byte in stdin().bytes() {
         match byte {
-            Ok(b) => {
-                print!("{}", b as char);
+            Ok(byte) => {
+                let char = byte as char;
+
+                if char.is_control() {
+                    println!("{:?} \r", byte);
+                } else {
+                    println!("{:?} ({})\r", byte, char);
+                }
+
+                if byte == to_ctrl_byte('q') {
+                    break;
+                }
             }
-            Err(e) => {
-                eprintln!("{}", e);
-            }
+            Err(e) => finish_with_error(e),
         }
     }
 }
