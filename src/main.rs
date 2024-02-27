b02cd1002c35c40f2611ecb6196de8bb8cb83b87
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-27 19:10:23 -0300

    feat: add initial read loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::io::{stdin, Read};
+
 fn main() {
-    println!("Hello, world!");
+    for byte in stdin().bytes() {
+        match byte {
+            Ok(b) => {
+                print!("{}", b as char);
+            }
+            Err(e) => {
+                eprintln!("{}", e);
+            }
+        }
+    }
 }
