f79dceebf23eb3f59ec4caf57043feeb58f41198
Victor Gutierrez <victorgutierrezgomes@gmail.com>
2024-02-29 16:41:27 -0300

    feat: add grapheme handling

--- src/row.rs
+++ src/row.rs
@@ -1,13 +1,17 @@
 use std::cmp;
 
+use unicode_segmentation::UnicodeSegmentation;
+
 pub struct Row {
     string: String,
+    len: usize
 }
 
 impl From<&str> for Row {
     fn from(slice: &str) -> Self {
         Self {
             string: String::from(slice),
+            len: 0
         }
     }
 }
@@ -18,14 +22,29 @@ impl Row {
         let end = cmp::min(end, self.string.len());
         let start = cmp::min(start, end);
 
-        self.string.get(start..end).unwrap_or_default().to_string()
+        let mut result = String::new();
+
+        for grapheme in self.string[..]
+            .graphemes(true)
+            .skip(start)
+            .take(end - start)
+        {
+            // Handle tabs
+            if grapheme == "\t" {
+                result.push_str(" ")
+            } else {
+                result.push_str(grapheme)
+            }
+        }
+
+        result
     }
 
     pub fn len(&self) -> usize {
-        self.string.len()
+        self.len
     }
 
     pub fn is_empty(&self) -> bool {
-        self.string.is_empty()
+        self.len == 0
     }
 }
